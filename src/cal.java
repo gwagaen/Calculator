3d219d222bb3173da90477018598ca0f154e9f6a
gwagaen <90993599+gwagaen@users.noreply.github.com>
2021-09-19 10:30:00 +0900

    Initial commit

---
+++ src/cal.java
@@ -0,0 +1,41 @@
+import java.util.regex.*;
+import java.util.Scanner;
+import java.lang.*;
+public class cal {
+    public static void main(String[] args) {
+        System.out.println("Calculator");
+        Scanner inputstr = new Scanner(System.in);
+        System.out.print("Input: ");
+        String actualString = inputstr.nextLine();
+        actualString = actualString.replaceAll("\\s+", "");
+
+        if (actualString.contains("+")) {
+            Pattern pattern = Pattern.compile("\\+");
+            String[] array = pattern.split(actualString, 0);
+            float res = Integer.valueOf(array[0]) + Integer.valueOf(array[1]);
+            System.out.println(res);
+        }
+        if (actualString.contains("-")) {
+            Pattern pattern = Pattern.compile("\\-");
+            String[] array = pattern.split(actualString, 0);
+            float res = Integer.valueOf(array[0]) - Integer.valueOf(array[1]);
+            System.out.println(res);
+        }
+        if (actualString.contains("*")) {
+            Pattern pattern = Pattern.compile("\\*");
+            String[] array = pattern.split(actualString, 0);
+            float res = Integer.valueOf(array[0]) * Integer.valueOf(array[1]);
+            System.out.println(res);
+        }
+        if (actualString.contains("/")) {
+            Pattern pattern = Pattern.compile("\\/");
+            String[] array = pattern.split(actualString, 0);
+            float res = Integer.valueOf(array[0]) / Integer.valueOf(array[1]);
+            System.out.println(res);
+        }
+        else {
+            System.out.println("Please, try again!");
+        }
+
+    }
+}
